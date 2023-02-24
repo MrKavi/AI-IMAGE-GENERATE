4b671bfccd0b2b41eefe8a9ffb0044f8295a88c2
kavin <kvigasyal@gmail.com>
2023-02-24 14:41:26 +0530

    UI updated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { preview } from "../assets";
-import { FormField } from "../components";
+import { FormField, Loader } from "../components";
+import { getRandomPrompt } from "../utils";
 
 const CreatePost = () => {
   const navigate = useNavigate();
@@ -9,9 +10,15 @@ const CreatePost = () => {
   const [generatingImg, setGenerationImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {};
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.vlaue });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -36,13 +43,60 @@ const CreatePost = () => {
             lableName="Prompt"
             type="text"
             name="prompt"
-            placeholder="A Space Shuttle flying above Cape Town, digital art"
+            placeholder={form.prompt}
             vlaue={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
-          <div className="relative"></div>
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div
+                className="absolute inset-0 z-0 flex justify-center items-center
+            bg-[rgba(0,0,0,0.5)] rounded-lg
+            "
+              >
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto 
+          px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating Image..." : "Generate Image"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created Image you want, You can share with friends....
+          </p>
+          <button
+            className="mt-3 text-white bg-[#6469ff] font-medium text-sm
+          rounded-md w-full sm:w-auto px-5 py-2.5 text-center
+          "
+            type="submit"
+          >
+            {loading ? "Sharing.." : "Share with friends"}
+          </button>
         </div>
       </form>
     </section>
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,8 +1,8 @@
 import { surpriseMePrompts } from "../constants";
 
-export function getRandomPrompts(prompt) {
+export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
-  if (randomPrompt === prompt) return getRandomPrompts(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
   return randomPrompt;
 }
